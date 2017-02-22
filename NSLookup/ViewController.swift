119e8aedec56cc637f5eeae054d94fe6ca00b97f
Kojirou <Kojirouhtc@gmail.com>
2017-02-22 17:23:13 +0800

    Initial Commit

---
+++ NSLookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSLookup
+//
+//  Created by 王宇 on 2017/2/22.
+//  Copyright © 2017年 Putotyra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
